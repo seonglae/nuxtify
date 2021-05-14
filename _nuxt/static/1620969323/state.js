251dceaa9abcbb8cdf01052cbde46e0cdea79b40
seonglae <sungle3737@gmail.com>
2021-05-14 05:22:59 +0000

    Deploying to gh-pages from @ seonglae/nuxtify@39f4a6268ff6f30c09fcd7438854f412ed269ced 🚀

--- _nuxt/static/1620969323/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxtify\u002F_nuxt\u002Fstatic\u002F1620969323",layout:"default",error:a,state:{},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fnuxtify\u002F",assetsPath:"\u002Fnuxtify\u002F_nuxt\u002F",cdnURL:a}}}}(null));
