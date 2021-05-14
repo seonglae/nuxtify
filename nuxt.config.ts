bce19656806afdae41e77a784b8a69c0605871cc
seonglae <sungle3737@gmail.com>
2021-05-14 16:45:00 +0900

    dynamic favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,7 +38,7 @@ const config: NuxtConfig = {
       { hid: 'description', name: 'description', content: pkg.description },
       { hid: 'keywords', name: 'keywords', content: KEYWORKDS.join() },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: process.env.BASE ? `/${pkg.name}/favicon.svg` : '/favicon.svg' }],
   },
   sitemap: { hostname: HOST },
 
